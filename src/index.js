273d870d16948517ba44b4775457d74bb60ad1c3
Daniela Cordoba <dacorac43@gmail.com>
2023-12-09 17:50:42 +1000

    Basic methods to create a new file in google drive folder

--- src/index.js
+++ src/index.js
@@ -1,8 +1,41 @@
+
+import * as path from 'path';
 import dotenv from 'dotenv';
+import { GoogleDriveService } from './services/googleDrive.service.js';
+import { fileURLToPath } from 'url';
 
 dotenv.config();
 
-console.log(process.env.GOOGLE_DRIVE_CLIENT_ID);
-console.log(process.env.GOOGLE_DRIVE_CLIENT_SECRET);
-console.log(process.env.GOOGLE_DRIVE_REDIRECT_URI);
-console.log(process.env.GOOGLE_DRIVE_REFRESH_TOKEN);
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const driveClientId = process.env.GOOGLE_DRIVE_CLIENT_ID || '';
+const driveClientSecret = process.env.GOOGLE_DRIVE_CLIENT_SECRET || '';
+const driveRedirectUri = process.env.GOOGLE_DRIVE_REDIRECT_URI || '';
+const driveRefreshToken = process.env.GOOGLE_DRIVE_REFRESH_TOKEN || '';
+
+const googleDriveService = new GoogleDriveService(driveClientId, driveClientSecret, driveRedirectUri, driveRefreshToken);
+
+// Define the file to upload
+const filePath = path.resolve(__dirname, '../public/image.png');
+const fileName = 'image';
+const folderName = 'pictures';
+const mimeType = 'image/jpg'
+
+
+let folder = await googleDriveService.searchFolder(folderName).catch((error) => {
+  console.error(error);
+  return null;
+});
+
+if (!folder) {
+  folder = await googleDriveService.createFolder(folderName);
+}
+
+// Upload the file to Google Drive
+googleDriveService.saveFile(fileName, filePath, mimeType, folder.id).catch((error) => {
+    console.error(error);
+    var div = document.getElementById('result');
+    div.innerHTML += `<p>${error}</p>`;
+});
+
